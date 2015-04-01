85c73af49479676e9f4bb36e3b6f2fddbb284b58
Amir Lellouche <lalosher@gmail.com>
2015-04-01 18:16:29 +0300

    hack to fix turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,12 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require jquery
+// require turbolinks
 //= require materialize-sprockets
 //= require_tree .
+
 $(document).ready(function() {
     $('select').material_select();
     $('.tooltipped').tooltip({delay: 50});
-
 });
